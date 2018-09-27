ba8f7232876f717041f54f8b78177900c8bce472
Ewa Baranowska <ewa.baranowska4@wp.pl>
2018-09-27 07:53:22 +0200

    Initial commit
    First version of ceterisParibusD3 JS library

---
+++ ceterisParibusD3/README.md
@@ -0,0 +1,8 @@
+# ceterisParibusD3
+
+JavaScript library ceterisParibusD3 for interactive plots for R package called ceterisParibus.
+
+Folder includes:
+
+- main file with JS library/code (ceterisParibusD3.js)
+- files for example of usage in pure JS (example.index, exemplaryData.js, exemplaryData_obs.js).
---
+++ ceterisParibusD3/exemplaryData_obs.js
@@ -0,0 +1,26 @@
+exemplaryData_obs =  [
+  {
+    "m2.price": 4397,
+    "construction.year": 2005,
+    "surface": 20,
+    "floor": 3,
+    "no.rooms": 2,
+    "district": "Wola",
+    "_yhat_": 4094.3828,
+    "_y_": 4397,
+    "_label_": "randomForest",
+    "_ids_": "1958"
+  },
+  {
+    "m2.price": 3126,
+    "construction.year": 1997,
+    "surface": 147,
+    "floor": 1,
+    "no.rooms": 6,
+    "district": "Ursus",
+    "_yhat_": 3093.0725,
+    "_y_": 3126,
+    "_label_": "randomForest",
+    "_ids_": "1353"
+  }
+]
